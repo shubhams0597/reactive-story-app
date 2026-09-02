77419894664c5757ff02ca551cbd95f111355b9e
shubhams0597 <shubhamsakpal005@gmail.com>
2026-09-03 00:27:21 +0530

    Bundled ESM image assets

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,45 +1,55 @@
+import slide1 from '../assets/slide1.png';
+import slide2 from '../assets/slide2.png';
+import slide3 from '../assets/slide3.png';
+import slide4 from '../assets/slide4.png';
+import slide5 from '../assets/slide5.png';
+import slide6 from '../assets/slide6.png';
+import slide7 from '../assets/slide7.png';
+import endingMarried from '../assets/ending_married.png';
+import endingOldAge from '../assets/ending_oldage.png';
+
 export const storySlides = [
   {
     id: 1,
     title: "The Beginning",
     caption: "It all started with a simple spark... ✨",
-    image: "./assets/slide1.png"
+    image: slide1
   },
   {
     id: 2,
     title: "Cozy Laughs",
     caption: "Endless late-night talks & sweet moments together ☕",
-    image: "./assets/slide2.png"
+    image: slide2
   },
   {
     id: 3,
     title: "Shelter in Rain",
     caption: "Being each other's quiet warm shelter in every storm 🌧️",
-    image: "./assets/slide3.png"
+    image: slide3
   },
   {
     id: 4,
     title: "Adventures Ahead",
     caption: "Exploring breathtaking sunsets & hand-in-hand adventures 🌄",
-    image: "./assets/slide4.png"
+    image: slide4
   },
   {
     id: 5,
     title: "Little Love Notes",
     caption: "The sweet unspoken little gestures that mean everything 💌",
-    image: "./assets/slide5.png"
+    image: slide5
   },
   {
     id: 6,
     title: "Through Thick & Thin",
     caption: "Standing side-by-side through every season of life ☂️",
-    image: "./assets/slide6.png"
+    image: slide6
   },
   {
     id: 7,
     title: "Looking Ahead",
     caption: "Looking into the starry sky... ready for our future? 🌌",
-    image: "./assets/slide7.png"
+    image: slide7
   }
 ];
 
@@ -48,24 +58,24 @@ export const happyEndingBeats = [
     id: 1,
     icon: "💍",
     text: "We will get married & celebrate our love",
-    image: "./assets/ending_married.png"
+    image: endingMarried
   },
   {
     id: 2,
     icon: "👶",
     text: "We will have kids & fill our home with laughter",
-    image: "./assets/ending_married.png"
+    image: endingMarried
   },
   {
     id: 3,
     icon: "✨",
     text: "We will live a very good and happy life together",
-    image: "./assets/ending_married.png"
+    image: endingMarried
   },
   {
     id: 4,
     icon: "👵👨‍🦳",
     text: "And at old age we will smile & say... We Made It.",
-    image: "./assets/ending_oldage.png"
+    image: endingOldAge
   }
 ];
